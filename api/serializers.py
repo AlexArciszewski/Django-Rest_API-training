77edec6f8870f7bdb94b10195d415d79971ef226
AlexArciszewski <arciszew@yahoo.com>
2024-04-09 00:01:49 +0200

    Preparing main files and api's files cleaning

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
@@ -10,8 +10,3 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 # Create your views here.
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import User
 from rest_framework import permissions, viewsets
 
-from api.serializers import GroupSerializer, UserSerializer
+from api.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,13 +11,3 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all().order_by('name')
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
